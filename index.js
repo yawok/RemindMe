027b2e987aca35581dce2cb9c5d9b96c7fe29ab3
yaw.o.k <obengky72@gmail.com>
2024-03-21 10:46:01 +0100

    Implement adding current tab functionality

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ let ulEl = document.getElementById("ul-el")
 const inputEl = document.getElementById("input-el")
 const saveButton = document.getElementById("save-btn")
 const deleteButton = document.getElementById("delete-btn")
+const addTabButton = document.getElementById("tab-btn")
 let links = []
 const savedLinks = JSON.parse(localStorage.getItem("links"))
-
+saveButton.disaled = true
 if (savedLinks) {
     links = savedLinks
+    render(links)
+}
+
+function addToList(link) {
+    links.push(link)
+    localStorage.setItem('links', JSON.stringify(links))
     render(links)
 }
 
@@ -15,7 +22,7 @@ function render(aList) {
     for (i in aList) {
         linksList += `
         <li>
-            <a target='_blank' href='https://${aList[i]}'>
+            <a target='_blank' href='${aList[i]}'>
                 ${aList[i]}
             </a>
         </li>`
@@ -26,10 +33,8 @@ function render(aList) {
 saveButton.addEventListener("click", () => {
     let value = inputEl.value
     if (value != "") {
-        links.push(value)
-        localStorage.setItem('links', JSON.stringify(links))
+        addToList(value)
         inputEl.value = ""
-        render(links)
     }
 })
 
@@ -37,4 +42,10 @@ deleteButton.addEventListener("dblclick", () => {
     localStorage.clear()
     links = []
     render(links)
+})
+
+addTabButton.addEventListener("click", () => {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+        addToList(tabs[0].url)
+    })
 })
